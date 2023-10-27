d275643a46b7739d6663c76f540477433cc88931
Koushik <koushik899@gmail.com>
2023-10-27 22:55:28 +0530

    save

--- src/components/RestaurantCatagory.js
+++ src/components/RestaurantCatagory.js
@@ -1,10 +1,10 @@
 const RestaurantCatagory=(props)=>{
-    console.log("props",props);
+    console.log("props",props.data);
     return(
         <div>
-            <div className="w-6/12 mx-auto my-6 bg-gray-50">
-                <span>{props.data.title}</span>
-                {/* <span>‘:smile’ </span> */}
+            <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4 flex justify-between">
+                <span className="font-bold text-lg">{props.data.title } ({props.data.itemCards.length})</span>
+                 <span>⬇️</span> 
 
             </div>
         </div>
